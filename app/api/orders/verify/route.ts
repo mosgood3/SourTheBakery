4df4158915066bbd27126e8b65f4be6e209625bf
mosgood3 <mattosgood7@gmail.com>
2025-07-10 14:04:59 -0400

    progress

--- app/api/orders/verify/route.ts
+++ app/api/orders/verify/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { getOrders } from '../../lib/products';
+import { getOrders } from '../../../lib/products';
 
 export async function POST(req: NextRequest) {
   try {
