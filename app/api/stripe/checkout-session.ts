2d2d8d751ea1b8695c57c4fafbb6e3cf71422283
mosgood3 <mattosgood7@gmail.com>
2025-07-17 14:13:13 -0400

    progress

--- app/api/stripe/checkout-session.ts
+++ app/api/stripe/checkout-session.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from 'next/server';
 import Stripe from 'stripe';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
-  apiVersion: '2022-11-15',
+  apiVersion: '2025-06-30.basil',
 });
 
 export async function POST(req: NextRequest) {
